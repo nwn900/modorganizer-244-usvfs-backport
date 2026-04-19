5fcdfc86ef54a0333541d6ef95c9e34abc624a54
nwn900 <243597087+nwn900@users.noreply.github.com>
2026-04-19 13:49:18 +0200

    fix: restore asmjit include root for 2.5.2

--- ASSEMBLER/prepare-usvfs-source.ps1
+++ ASSEMBLER/prepare-usvfs-source.ps1
@@ -1214,10 +1214,16 @@ Get-ChildItem -Path $sourceDir -Include '*.vcxproj', '*.props' -Recurse | ForEac
                 'C:\Games\MO2\build\usvfs_clean\udis86\libudis86;',
                 '..\udis86\libudis86;')
         }
-        if ($asmjitFlatHeader -and $vcxText.Contains('..\asmjit\src\asmjit;')) {
-            $vcxText = $vcxText.Replace('..\asmjit\src\asmjit;', '..\asmjit\src;')
-        }
-        if ($asmjitNestedHeader -and -not $vcxText.Contains('..\asmjit\src\asmjit;')) {
+        if ($asmjitNestedHeader) {
+            if ($vcxText.Contains('..\asmjit\src\asmjit;')) {
+                $vcxText = $vcxText.Replace('..\asmjit\src\asmjit;', '..\asmjit\src;')
+            } elseif ($vcxText -match '<AdditionalIncludeDirectories>' -and $vcxText -notmatch '\.\.\\asmjit\\src;') {
+                $vcxText = [regex]::Replace(
+                    $vcxText,
+                    '(?i)(<AdditionalIncludeDirectories>)',
+                    '${1}..\asmjit\src;')
+            }
+        } elseif ($asmjitFlatHeader -and -not $vcxText.Contains('..\asmjit\src\asmjit;')) {
             if ($vcxText.Contains('..\asmjit\src;')) {
                 $vcxText = $vcxText.Replace(
                     '..\asmjit\src;',
