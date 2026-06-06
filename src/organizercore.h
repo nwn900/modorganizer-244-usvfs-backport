051b1521b3ab26e9d2f3e27a5dab3179865e2f2d
nwn900 <nwn900@users.noreply.github.com>
2026-06-06 18:25:06 +0200

    fix: qualify ModDataContent for stock uibase

--- src/organizercore.h
+++ src/organizercore.h
@@ -106,7 +106,7 @@ public:
   struct ModDataContentHolder
   {
 
-    using Content = ModDataContent::Content;
+    using Content = MOBase::ModDataContent::Content;
 
     /**
      * @return true if the hold list of contents is empty, false otherwise.
@@ -205,7 +205,7 @@ public:
     /**
      * @brief Construct a ModDataContentHold holding the given list of contents.
      */
-    ModDataContentHolder(std::vector<ModDataContent::Content> contents)
+    ModDataContentHolder(std::vector<MOBase::ModDataContent::Content> contents)
         : m_Contents(std::move(contents))
     {}
 
